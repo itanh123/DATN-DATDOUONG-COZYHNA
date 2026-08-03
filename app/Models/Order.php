a34eff575656e210f3ded2ded7a37f9a1ed34dcd
itanh123 <haianh13102006@gmail.com>
2026-08-03 22:40:12 +0700

    fix: add missing payment relationship to Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -60,6 +60,10 @@ class Order extends Model
         return $this->hasMany(OrderItem::class, 'order_id');
     }
 
+    public function payment() {
+        return $this->hasOne(Payment::class, 'order_id');
+    }
+
     public function getStatusLabelAttribute()
     {
         return match ($this->order_status) {
