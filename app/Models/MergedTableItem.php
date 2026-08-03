e04bf9a2795d358838f9b25ae4eb8ea7a4615448
itanh123 <haianh13102006@gmail.com>
2026-08-03 22:33:40 +0700

    fix: resolve remaining model-database mismatches (MergedTableItem timestamps, missing notifications table)

--- app/Models/MergedTableItem.php
+++ app/Models/MergedTableItem.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class MergedTableItem extends Model
 {
     protected $table = 'merged_table_items';
+    public $timestamps = false;
 
     protected $fillable = ['merged_table_id', 'table_id', 'is_primary'];
 
